5d0af9d34ae1924602a49f3ac93fa0ec24b51732
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-07-02 21:35:12 +0100

    prepare classes to divide game elements

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-import Player from "./Player.js"
-const canvas = document.getElementById("game")
-const ctx = canvas.getContext("2d")
+window.addEventListener('load', function(){
+    const canvas = document.getElementById("game")
+    const ctx = canvas.getContext('2d')
+    canvas.width = 500
+    canvas.height = 500
 
-const player = new Player(canvas. width / 2.2, canvas.height / 1.3)
+    class InputHandler {
 
-canvas.width = 550
-canvas.height = 600
+    }
 
-function gameLoop(){
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-}
+    class Projectile {
 
-setInterval(gameLoop, 1000 / 60)
+
+    }
+
+    class Particle {
+
+
+    }
+
+    class Player {
+
+
+    }
+
+    class Enemy {
+
+
+    }
+
+    class Layer {
+        // Not needed I think 
+    }
+
+    class Background {
+        // Not needed I think
+    }
+
+    class UI {
+        // Clock, timer and other info
+    }
+
+    class Game {
+        // Here all logic comes together
+    }
+})
