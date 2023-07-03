25a70d1ca64933c66c2f36af5c02fe7c4027ffa3
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-07-03 16:34:03 +0100

    create projectile class

--- script.js
+++ script.js
@@ -23,7 +23,23 @@ window.addEventListener('load', function(){
     }
 
     class Projectile {
-
+        constructor(game, x, y){
+            this.game = game
+            this.x = x
+            this.y = y
+            this.width = 10
+            this.height = 3
+            this.speed = 3
+            this.markedForDeletion = false
+        }
+        update(){
+            this.x += this.speed
+            if(this.x > this.game.width * 0.8) this.markedForDeletion = true
+        }
+        draw(context){
+            context.fillStyle = 'yellow'
+            fillRect(this.x, this.y, this.width, this.height)
+        }
 
     }
 
@@ -48,6 +64,7 @@ window.addEventListener('load', function(){
             this.y += this.speedY
         }
         draw(context){
+            context.fillStyle = 'black'
             context.fillRect(this.x, this.y, this.width, this.height) 
         }
 
