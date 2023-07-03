b4a0059159713ae6f58683ef88ebd1d00f1d3cd5
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-07-03 19:37:40 +0100

    create checkCollision function

--- script.js
+++ script.js
@@ -154,6 +154,14 @@ window.addEventListener('load', function(){
         addEnemy(){
             this.enemies.push(new Enemy(this))
         }
+        checkCollision(rect1, rect2){
+            return (
+                rect1.x < rect2.x + rect2.width && 
+                rect1.x + rect1.width > rect2.x &&
+                rect1.y < rect2.y + rect2.height &&
+                rect1.height + rect1.y > rect2.y
+            )
+        }
     }
     const game = new Game(canvas.width, canvas.height)
     let lastTime = 0
